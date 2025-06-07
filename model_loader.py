f3d0436a3139f05c872cdbe82607aba966427553
smalldatabrains <smalldatabrains@gmail.com>
2025-06-07 08:58:45 +0200

    adding model chart to tensorboard

--- model_loader.py
+++ model_loader.py
@@ -1,15 +1,16 @@
 # Basic import
 import torch
 from torch import nn
-import datetime
-import pandas as pd
-import os
-from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+from classifier import ConvNet
 
-model = nn.Sequential(
-    nn.Conv2d(in_channels=3,out_channels=24,kernel_size=3,stride=1,padding=2),
-    nn.ReLU(),
-    nn.MaxPool2d(2,2)
-    nn.Flatten(),
-    nn.Linear(10*14*14,10)
-)
+model = ConvNet()
+model.load_state_dict(torch.load("ConvNet.pth"))
+
+model.eval()
+
+dummy_input = torch.randn(1,1,28,28)
+
+writer = SummaryWriter('runs/model_architecture')
+writer.add_graph(model,dummy_input)
+writer.close()
