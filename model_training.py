788d23899b61a5eab86119232e42f1a8888b5058
smalldatabrains <smalldatabrains@gmail.com>
2025-06-12 12:05:03 +0200

    flatten layer function

--- model_training.py
+++ model_training.py
@@ -75,7 +75,7 @@ class SimpleNet(nn.Module):
         x=self.convolution(x) # [B, C, H, W ]
         x=self.relu(x)
         x=self.pool(x)
-        x=x.view(x.size(0),-1)
+        x=torch.flatten(x,1)
         x=self.fc1(x)
         x=self.relu(x)
         x=self.fc2(x)
